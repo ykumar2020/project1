25a756dd6d7c40b84b2b21504d65f94d7415dde2
galae <galae@DESKTOP-LMENB1R.myfiosgateway.com>
2022-01-01 23:07:03 -0500

    Initial Project Commit

---
+++ chapter1/src/test/Program1.java
@@ -0,0 +1,7 @@
+package test;
+
+public class Program1 {
+	public static void main(String[] args) {
+		System.out.println("Computer Programming in Java: from the very basics to Android apps");
+	}
+}
